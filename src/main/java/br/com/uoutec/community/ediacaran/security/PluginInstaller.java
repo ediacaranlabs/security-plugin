43513649a7f3c54a46547d97d04dbe7377489ca7
Afonso <afonso.rbn@gmail.com>
2018-12-14 18:01:43 -0200

    #

---
+++ src/main/java/br/com/uoutec/community/ediacaran/security/PluginInstaller.java
@@ -0,0 +1,7 @@
+package br.com.uoutec.community.ediacaran.security;
+
+import br.com.uoutec.community.ediacaran.system.AbstractPluginInstaller;
+
+public class PluginInstaller extends AbstractPluginInstaller{
+	
+}
