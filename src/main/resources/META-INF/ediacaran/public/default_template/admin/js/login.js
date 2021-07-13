f9ee4842748664c44d3e97e60b487975bd92a461
Afonso <afonso.rbn@gmail.com>
2021-07-13 11:09:33 -0300

    #

--- src/main/resources/META-INF/ediacaran/public/default_template/admin/js/login.js
+++
@@ -1,9 +0,0 @@
-$.AppContext.onload(function (){
-
-	$('input').iCheck({
-		checkboxClass: 'icheckbox_square-blue',
-		radioClass: 'iradio_square-blue',
-		increaseArea: '20%' // optional
-	});
-	
-});
