5a619ab2f3980a7f22966488fa14f3072e70549f
Afonso <afonso.rbn@gmail.com>
2021-11-07 19:37:11 -0200

    #

--- src/main/java/br/com/uoutec/community/ediacaran/security/PluginInstaller.java
+++ src/main/java/br/com/uoutec/community/ediacaran/security/PluginInstaller.java
@@ -4,14 +4,19 @@ import br.com.uoutec.application.ClassUtil;
 import br.com.uoutec.community.ediacaran.AbstractPlugin;
 import br.com.uoutec.community.ediacaran.EdiacaranEventListener;
 import br.com.uoutec.community.ediacaran.EdiacaranListenerManager;
+import br.com.uoutec.community.ediacaran.core.security.AuthorizationManager;
 import br.com.uoutec.community.ediacaran.plugins.EntityContextPlugin;
 import br.com.uoutec.community.ediacaran.plugins.PluginConfiguration;
 import br.com.uoutec.community.ediacaran.plugins.PluginException;
+import br.com.uoutec.community.ediacaran.plugins.PluginType;
 import br.com.uoutec.community.ediacaran.security.pub.WebSecurityManagerPlugin;
+import br.com.uoutec.community.ediacaran.security.pub.test.FileAuthenticationProvider;
 
 public class PluginInstaller extends AbstractPlugin{
 	
 	public static final String CONFIGURATION_LISTENER = "listener";
+
+	public static final String USE_DEFAULT_AUTHENTICATION_PROVIDER_PROPERTY = "default_authentication";
 
 	private EdiacaranEventListener configurationListener;
 	
@@ -31,6 +36,18 @@ public class PluginInstaller extends AbstractPlugin{
 			.addConstraint("/admin/*")
 				.addRole("user")
 			.form("/login", "/login?error=true");
+		
+		PluginType pluginType = EntityContextPlugin.getEntity(PluginType.class);
+		
+		boolean userDefaultAuthenticationProvider = 
+				pluginType.getConfiguration().getBoolean(USE_DEFAULT_AUTHENTICATION_PROVIDER_PROPERTY);
+		
+		if(userDefaultAuthenticationProvider) {
+			FileAuthenticationProvider fup = EntityContextPlugin.getEntity(FileAuthenticationProvider.class);
+			AuthorizationManager sm = EntityContextPlugin.getEntity(AuthorizationManager.class);
+			sm.registerAuthenticationProvider(fup);
+		}
+		
 	}
 	
 	@SuppressWarnings("unchecked")
@@ -64,6 +81,17 @@ public class PluginInstaller extends AbstractPlugin{
 	@Override
 	public void uninstall() throws Throwable {
 		removeListeners();
+		
+		PluginType pluginType = EntityContextPlugin.getEntity(PluginType.class);
+		
+		boolean userDefaultAuthenticationProvider = 
+				pluginType.getConfiguration().getBoolean(USE_DEFAULT_AUTHENTICATION_PROVIDER_PROPERTY);
+		
+		if(userDefaultAuthenticationProvider) {
+			AuthorizationManager sm = EntityContextPlugin.getEntity(AuthorizationManager.class);
+			sm.registerAuthenticationProvider(null);
+		}
+		
 	}
 	
 	private void removeListeners() {
---
+++ src/main/java/br/com/uoutec/community/ediacaran/security/pub/test/FilePrincipal.java
@@ -0,0 +1,31 @@
+package br.com.uoutec.community.ediacaran.security.pub.test;
+
+import java.util.Set;
+
+import br.com.uoutec.community.ediacaran.core.security.Principal;
+
+public class FilePrincipal implements Principal{
+
+	private final FileUser user;
+	
+	public FilePrincipal(FileUser user) {
+		super();
+		this.user = user;
+	}
+
+	@Override
+	public Object getUserPrincipal() {
+		return user.getName();
+	}
+
+	@Override
+	public Set<String> getRoles() {
+		return user.getRoles();
+	}
+
+	@Override
+	public Set<String> getStringPermissions() {
+		return user.getStringPermissions();
+	}
+
+}
