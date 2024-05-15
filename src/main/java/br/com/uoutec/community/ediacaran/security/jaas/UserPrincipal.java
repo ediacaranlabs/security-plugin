9fcfbcde0bf5fa83c71776c1993045baef44f329
Brandao <Brandao@git.uoutec.com.br>
2024-05-15 20:36:14 -0300

    default

--- src/main/java/br/com/uoutec/community/ediacaran/security/jaas/UserPrincipal.java
+++ src/main/java/br/com/uoutec/community/ediacaran/security/jaas/UserPrincipal.java
@@ -1,11 +1,17 @@
 package br.com.uoutec.community.ediacaran.security.jaas;
 
+import br.com.uoutec.community.ediacaran.security.AuthenticatedSubject;
+import br.com.uoutec.community.ediacaran.security.Subject;
+
 public class UserPrincipal implements java.security.Principal {
 
 	private br.com.uoutec.community.ediacaran.security.Principal userPrincipal;
 
+	private AuthenticatedSubject authenticatedSubject;
+	
 	public UserPrincipal(br.com.uoutec.community.ediacaran.security.Principal userPrincipal) {
 		this.userPrincipal = userPrincipal;
+		this.authenticatedSubject = new AuthenticatedSubject(userPrincipal);
 	}
 
 	@Override
@@ -15,6 +21,10 @@ public class UserPrincipal implements java.security.Principal {
 
 	public br.com.uoutec.community.ediacaran.security.Principal getUserPrincipal(){
 		return userPrincipal;
+	}
+
+	public Subject getSubject(){
+		return authenticatedSubject;
 	}
 	
 }
